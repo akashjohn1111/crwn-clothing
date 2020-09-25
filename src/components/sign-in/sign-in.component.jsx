80fbf9a35efe8d2711bf2c1fcd29e824c2aea94e
Akash <akash@Akashs-MacBook-Pro.local>
2020-09-25 19:55:37 +0530

    adding email  sign in in left side

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -62,7 +62,7 @@ catch(error)
           />
           <div className='buttons'>
             <CustomButton type='submit'> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
               Sign in with Google
             </CustomButton>
           </div>
