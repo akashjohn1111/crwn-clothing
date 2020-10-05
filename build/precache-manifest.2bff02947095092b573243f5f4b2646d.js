e9051b318db3d4f78ee07b0c0f33dce41cff3866
Akash <akash@Akashs-MacBook-Pro.local>
2020-10-05 17:05:33 +0530

    removed logger

---
+++ build/precache-manifest.2bff02947095092b573243f5f4b2646d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9073b60bb0584e5547b91d99a608830b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "451563754c8f3bc71349",
+    "url": "/static/css/main.833b1320.chunk.css"
+  },
+  {
+    "revision": "ced1ba98ce56f3bdaadf",
+    "url": "/static/js/2.e369285b.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.e369285b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451563754c8f3bc71349",
+    "url": "/static/js/main.7d430615.chunk.js"
+  },
+  {
+    "revision": "6bedc7d4e72b37a14961",
+    "url": "/static/js/runtime-main.059467c3.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/11.2 shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  }
+]);
